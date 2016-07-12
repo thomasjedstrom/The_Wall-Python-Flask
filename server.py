0dd587667dbef7dcdcdc2be4263370846cea291b
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-07-12 11:24:50 -0500

    login and registration page is functional.  On the wall: navbar exists, not fully functional.  Posts are displayed.  Author and timestamp displayed.

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import re
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.secret_key = 'KeepItSecretKeepItSafe'
-mysql = MySQLConnector(app, 'login_and_registration')
+mysql = MySQLConnector(app, 'the_wall')
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
 noNumbers = re.compile(r'^[^0-9]+$')
@@ -18,27 +18,15 @@ def index():
 def login():
     user_name = request.form['user_name']
     password = request.form['pw']
-    user_query = "SELECT * FROM registered_users WHERE user_name = :user_name LIMIT 1"
+    user_query = "SELECT * FROM users WHERE user_name = :user_name LIMIT 1"
     query_data = { 'user_name': user_name }
     user = mysql.query_db(user_query, query_data) # user will be returned in a list
     if bcrypt.check_password_hash(user[0]['pw_hash'], password):
         session['name'] = user_name
-        print session['name']
-        return redirect('/registereduser')
+        return redirect('/wall')
     else:
         flash('Invalid user or password', 'error')
         return redirect('/')
-
-
-@app.route('/registereduser')
-def registereduser():
-    query = "SELECT * FROM registered_users"
-    all_users = mysql.query_db(query)
-    return render_template('registereduser.html', all_users=all_users, name=session['name'])
-
-
-
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -53,7 +41,7 @@ def register():
     if len(user_name) < 1:
         errors.append('Username must be at least 2 characters')
     else:
-        username_query = "SELECT user_name FROM registered_users WHERE user_name = :user_name LIMIT 1"
+        username_query = "SELECT user_name FROM users WHERE user_name = :user_name LIMIT 1"
         query_username = { 'user_name': user_name }
         existencematch = mysql.query_db(username_query, query_username)
         if len(existencematch) != 0:
@@ -65,7 +53,7 @@ def register():
     if not EMAIL_REGEX.match(request.form['email']):
         errors.append('Invalid email address')
     else:
-        email_query = "SELECT email FROM registered_users WHERE email = :email LIMIT 1"
+        email_query = "SELECT email FROM users WHERE email = :email LIMIT 1"
         query_email = { 'email': email }
         existencematch = mysql.query_db(email_query, query_email)
         if len(existencematch) != 0:
@@ -82,7 +70,7 @@ def register():
         return redirect('/')
     else:
         pw_hash = bcrypt.generate_password_hash(password)
-        insert_query = "INSERT INTO registered_users (user_name, first_name, last_name, email, pw_hash, created_at, updated_at) VALUES (:user_name, :first_name, :last_name, :email, :pw_hash, NOW(), NOW())"
+        insert_query = "INSERT INTO users (user_name, first_name, last_name, email, pw_hash, created_at, updated_at) VALUES (:user_name, :first_name, :last_name, :email, :pw_hash, NOW(), NOW())"
         query_data = { 'user_name': user_name,
                         'first_name': first_name,
                         'last_name': last_name,
@@ -90,6 +78,17 @@ def register():
                         'pw_hash': pw_hash 
                         }
         mysql.query_db(insert_query, query_data)
-        return redirect('/registereduser')
+        return redirect('/wall')
+
+@app.route('/wall')
+def wall():
+    post_query = "SELECT messages.message, messages.created_at, users.user_name FROM messages JOIN users ON messages.user_id=users.id"
+    all_messages = mysql.query_db(post_query)
+    return render_template('wall.html', all_messages=all_messages, name=session['name'])
+
+
+
+
+
 
 app.run(debug=True)
